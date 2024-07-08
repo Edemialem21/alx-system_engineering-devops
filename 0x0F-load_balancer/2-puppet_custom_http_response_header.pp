8aed2c96304ed9def0fb786de7796f84eccb1d80
Edemialem21 <21edemialem@gmail.com>
2024-07-08 18:09:54 +0300

    puppet http respoonse

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+# Puppet script for configuring nginx webserver and adding http custom header.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  service nginx restart',
+  sudo sed -i "/listen 80 default_server;/a\\
+    add_header X-Served-By $HOSTNAME;\\
+" /etc/nginx/sites-available/default
+  provider => shell,
+}
